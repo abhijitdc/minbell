3ae1244a7dab567d1dd75449f5af42a9f9d9e40c
abhijitdc <abhisaxj@gmail.com>
2025-09-13 17:47:05 -0700

    base path fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/minbell/', // Set base path for GitHub Pages
   test: {
     globals: true,
     environment: 'jsdom',
