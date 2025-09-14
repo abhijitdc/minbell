4ec1e7c904e960f5d3dea3d31491b16bdc700fe4
abhijitdc <abhisaxj@gmail.com>
2025-09-13 19:35:08 -0700

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/', // Set base path for root domain deployment
+  build: {
+    assetsDir: 'assets', // Ensure assets are placed in 'assets' directory
+  },
   test: {
     globals: true,
     environment: 'jsdom',
